a7e5d3182a36e0a24aae865e8fd97ae2c0410d32
edsonfreitas <edsonfreitasarruda872@gmail.com>
2025-06-22 18:31:24 -0300

    fear: fixing vercel.json to vercel deploy

--- api/index.js
+++ api/index.js
@@ -1,20 +1,19 @@
-import Koa from 'koa';
-import Router from '@koa/router';
+import Koa from "koa";
+import Router from "@koa/router";
 
 const app = new Koa();
 const router = new Router();
 
-router.get('/hello', async ctx => {
-  ctx.body = '👋 Olá do /api/hello!';
+router.get("/hello", async (ctx) => {
+  ctx.body = "👋 Olá do /api/hello!";
 });
 
-router.get('/goodbye', async ctx => {
-  ctx.body = '👋 Tchauzinho do /api/goodbye!';
+router.get("/goodbye", async (ctx) => {
+  ctx.body = "👋 Tchauzinho do /api/goodbye!";
 });
 
 app.use(router.routes()).use(router.allowedMethods());
 
-export default (req, res) => {
-  app.callback()(req, res);
-};
+const handler = app.callback();
 
+export default (req, res) => handler(req, res);
